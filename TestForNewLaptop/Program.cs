2354712d83f5c55337e16b76eddf1c7bcad7f784
SimonLight001 <sipoe900@gmail.com>
2016-09-07 19:04:28 +0100

    Sync

--- TestForNewLaptop/Program.cs
+++ TestForNewLaptop/Program.cs
@@ -10,6 +10,8 @@ namespace TestForNewLaptop
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.Read();
         }
     }
 }
